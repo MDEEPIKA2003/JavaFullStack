ec6f296b76e354ab386490c9c3054ae727da5829
Deepika M <deepika2003@example.com>
2025-07-28 15:25:56 +0530

    first commit

---
+++ assignment/src/assessment/ArrayLists.java
@@ -0,0 +1,13 @@
+package assessment;
+import java.util.ArrayList; 
+import java.util.Arrays; 
+public class ArrayLists { 
+public static void  main(String[] args)  
+{ 
+String[]  my_array = new String[] {"Python", "JAVA", "PHP",  "Perl", 
+"C#", "C++"}; 
+ArrayList<String>  list = new 
+ArrayList<String>(Arrays.asList(my_array)); 
+System.out.println(list); 
+} 
+}
